ca40f57c913bd3d7790b0a79ae6551a48acaff11
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-16 18:43:18 +0100

    Update view_example.php

--- view_example.php
+++ view_example.php
@@ -2,7 +2,7 @@
 /*
 * Example of a HTML view to show, add, modify, or delete records for family members of a family for an user. 
 * Records are grouped by family, with relevant information like adres. 
-* The CSS classes allow for multiple instances on the same page while styling the content (instead of id which allows only one instance on the same page).
+* The CSS classes allow for multiple instances on the same page while styling the content (instead of id which allows only one instance on the same page.
 * Adding, modifying, or deleting a record will take the user to a form view, with the data automatically filled from the database if a record exists, otherwise a new unique record can be created
 * The header and footer and other HTML are in other views, where those and this view is loaded in through the Controller depending on the request the user made in the browser.
 * The data in the view comes from the database through the Model, and preparing the data for the view is done in the Controller.
