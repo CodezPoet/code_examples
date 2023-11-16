0b6317823a36dc7b22f07dd39943d6019eb957d3
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-16 19:52:28 +0100

    Update view_example.php

--- view_example.php
+++ view_example.php
@@ -1,9 +1,11 @@
 <?php
 /*
-* Example of a HTML View to show, add, modify, or delete records for family members of a family for an user. Records are grouped by family, with relevant information like adres
+* Example of a HTML View to show, add, modify, or delete records for family members of a family by an user. Records are grouped by family with relevant information like adres
 * Clicking on a link to add, edit, or delete loads a relevant form view, where the user can add, delete, or edit the data. 
-* The view is part of a HTML page content section, and part of a compilation of Views to build a complete HTML page. 
-* The view passes W3C and WCAG 2.0 Accessibility validation.
+* The view is loaded through a controller, and it works with a model to provide the data from the database
+* The view is part of a HTML page content section,in a compilation of Views to build a complete HTML page. 
+* The HTML of the view passes W3C and WCAG 2.0 Accessibility online validation.
+* The view uses CSS classes and ids as applicable with HTML elements so it can be styled with CSS
 */
 ?>
 <div id="family-wrapper">
