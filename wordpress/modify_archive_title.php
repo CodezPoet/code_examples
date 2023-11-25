a8debf9911e82926198eae63b561f7af0647d0d0
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-25 23:46:32 +0100

    Update modify_archive_title.php

--- wordpress/modify_archive_title.php
+++ wordpress/modify_archive_title.php
@@ -4,13 +4,13 @@
 add_filter('get_the_archive_title', 'modify_archive_title', 10, 1);
 
 // Function for add filter: Modify archive title for selected categories
-function modify_archive_title($title = '') 
+function modify_archive_title($title = '')
 {
-    
+
     $parent_cat_id = '';
     $parent_category_title = '';
     $archive_title = '';
-    $title = sanitize_text_field($title); 
+    $title = sanitize_text_field($title);
     $current_cat_info = get_category(get_query_var('cat'));
     $parent_cat_id = $current_cat_info->category_parent;
     if (744 == $parent_cat_id) {
@@ -28,12 +28,12 @@ function modify_archive_title($title = '')
     if (856 == $parent_cat_id) {
         $parent_category_title = 'Fashion News';
     }
-    if($title && !$parent_category_title) {
+    if ($title && !$parent_category_title) {
         $title = str_ireplace('Category:', '', $title);
         return $title;
     }
-    if($title && $parent_category_title) {
-       $archive_title = $title . ' ' . $parent_category_title;
-       return $archive_title;
+    if ($title && $parent_category_title) {
+        $archive_title = $title . ' ' . $parent_category_title;
+        return $archive_title;
     }
 }
