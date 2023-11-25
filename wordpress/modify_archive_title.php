4b024b6d0b7f7a191f88669710ac25bdee22d706
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-25 23:52:52 +0100

    Update modify_archive_title.php

--- wordpress/modify_archive_title.php
+++ wordpress/modify_archive_title.php
@@ -3,7 +3,7 @@
 // Add filter: Modify archive title for selected categories
 add_filter('get_the_archive_title', 'modify_archive_title', 10, 1);
 
-// Function for add filter: Modify archive title for selected categories
+// Function for add_filter('get_the_archive_title', 'modify_archive_title', 10, 1);
 function modify_archive_title($title = '')
 {
     $parent_cat_id = '';
