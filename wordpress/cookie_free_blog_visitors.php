47f4f075e50afe10388329128c4e5ebdb42be31d
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-25 23:49:08 +0100

    Update cookie_free_blog_visitors.php

--- wordpress/cookie_free_blog_visitors.php
+++ wordpress/cookie_free_blog_visitors.php
@@ -1,12 +1,13 @@
 <?php
 
-**
-* Some websites have stopped using cookies.  
-* With this code the WordPress blog doesn't use cookies for non logged in visitors.
-* Keep in mind third party content providers like YouTube may still set cookies in embedded content.
-*/
+/*
+ * Some websites have stopped using cookies.  
+ * With this code the WordPress blog doesn't use cookies for non logged in visitors.
+ * Keep in mind third party content providers like YouTube may still set cookies in embedded content.
+ */
 add_action('init', 'remove_blog_cookies');
 
+// function for add action to remove blog cookies
 function remove_blog_cookies()
 {
     if (!is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php') {
