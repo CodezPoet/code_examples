6e7aeefa27a92e4ac3ccf71ea720038cc3476fb8
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-25 23:52:18 +0100

    Update cookie_free_blog_visitors.php

--- wordpress/cookie_free_blog_visitors.php
+++ wordpress/cookie_free_blog_visitors.php
@@ -8,7 +8,7 @@
  */
 add_action('init', 'remove_blog_cookies');
 
-// function for add_action to remove blog cookies
+// function for add_action('init', 'remove_blog_cookies');
 function remove_blog_cookies()
 {
     if (!is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php') {
