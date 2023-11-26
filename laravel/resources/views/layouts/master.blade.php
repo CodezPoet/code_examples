ed5982c275de494ec29bc1325582e2b0857be809
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-26 15:52:44 +0100

    Update master.blade.php

--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
