7e29590cafc8fd72b71ce1979ae8260a97f00cde
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-26 14:06:05 +0100

    Update posts.blade.php
    Added html purifying

--- laravel/resources/views/layouts/posts.blade.php
+++ laravel/resources/views/layouts/posts.blade.php
@@ -6,10 +6,10 @@
 
 <div id="data-wrapper">
    <h2>Posts</h2>
-   @foreach ($posts as $post)
+   @foreach ($html_purified_posts as $html_purified_post)
    <div class="post">
-      <h3>{{ $post['title']['rendered'] }}</h3>
-      {!! $post['content']['rendered'] !!}
+      <h3>{{ $html_purified_post['title'] }}</h3>
+      {!! $html_purified_post['content'] !!}
    </div>
    @endforeach
 </div>
