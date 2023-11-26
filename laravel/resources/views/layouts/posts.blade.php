2034b093fae69dc0942aeeb169301eaf6d216d2e
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-26 15:40:14 +0100

    Update posts.blade.php

--- laravel/resources/views/layouts/posts.blade.php
+++ laravel/resources/views/layouts/posts.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div id="data-wrapper">
-   <h2>Posts</h2>
+   <h2>Archive</h2>
    @foreach ($html_purified_posts as $html_purified_post)
    <div class="post">
       <h3>{{ $html_purified_post['title'] }}</h3>
