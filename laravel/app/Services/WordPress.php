c741d3214f9ca36faa2461be944bc2e65eed8fb5
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-11-28 23:17:00 +0100

    Update WordPress.php

--- laravel/app/Services/WordPress.php
+++ laravel/app/Services/WordPress.php
@@ -11,21 +11,21 @@ class WordPress
      * Using Mews + HTML Purifier to sanitize HTML
      * 
      */
-    function get_posts()
+    function getPosts()
     {
         $response = Http::get(env('WP_SITE') . '/wp-json/wp/v2/posts');
         $posts = $response->json();
         foreach ($posts as $post) {
-            $html_purified_id = clean($post['id']);
-            $html_purified_title = clean($post['title']['rendered'], 'titles');
-            $html_purified_content = clean($post['content']['rendered']);
-            if (!empty($html_purified_id) && !empty($html_purified_title) && !empty($html_purified_content)) {
-                $html_purified_posts[$html_purified_id]['title'] = $html_purified_title;
-                $html_purified_posts[$html_purified_id]['content'] = $html_purified_content;
+            $htmlPurifiedId = clean($post['id']);
+            $htmlPurifiedTitle = clean($post['title']['rendered'], 'titles');
+            $htmlPurifiedContent = clean($post['content']['rendered']);
+            if (!empty($htmlPurifiedId) && !empty($htmlPurifiedTitle) && !empty($htmlPurifiedContent)) {
+                $htmlPurifiedPosts[$htmlPurifiedId]['title'] = $htmlPurifiedTitle;
+                $htmlPurifiedPosts[$htmlPurifiedId]['content'] = $htmlPurifiedContent;
             }
         }
-        if (isset($html_purified_posts)) {
-            return $html_purified_posts;
+        if (isset($htmlPurifiedPosts)) {
+            return $htmlPurifiedPosts;
         } else {
             return false;
         }
