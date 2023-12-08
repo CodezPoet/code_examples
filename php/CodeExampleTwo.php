8e984b802a27e63bc3d19f52cb80b0179c800671
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-08 01:41:35 +0100

    Update CodeExampleTwo.php

--- php/CodeExampleTwo.php
+++ php/CodeExampleTwo.php
@@ -1,15 +1,19 @@
 <?php
 
-namespace example;
+// Path/To/Example/CodeExampleTwo.php
+namespace Path\To\Example;
 
-/* 
-* Code example of data manipulation for arrays and output
-* The data is coming from a multiple join query in SQL and needs to be prepared for the wished HTML output
-*/
+use Path\To\Sanitize;
+use Path\To\OutputAdjustment;
+
 class CodeExampleTwo
 {
-    // take result from database for family records and organize the data in an array
-    function mtd_familie_onderhoud($records)
+    /*
+    * The data is coming from a multiple join query in SQL and needs to be prepared for the wished HTML output
+    * Check if the data is save and if is organize the data in an array for the HTML, else return false 
+    * This could be run as a Service for a Controller for example
+    */
+     function mtd_familie_overview($records)
     {
         $obj_sanitize = new Sanitize();
         $obj_output_adjustment = new OutputAdjustment;
@@ -17,26 +21,21 @@ class CodeExampleTwo
             foreach ($record as $key => $row) {
                 $key = $obj_sanitize->mtd_sfi($key);
                 $row = $obj_sanitize->mtd_sfi($row);
-                if (false !== $key && false !== $row) {
-                    $records[$key] = $row;
-                    if ('id' === $key || 'persoonsnaam' === $key || 'soort_lid' === $key || 'geboortedatum' === $key) {
-                        if ('geboortedatum' == $key) {
-                            $row = $obj_output_adjustment->mtd_human_readable_date($row);
-                        }
-                        $familieleden[$key] = $row;
+                if(false === $key && false === $row) {
+                    return false;
+                }
+                $records[$key] = $row;
+                if ('id' === $key || 'persoonsnaam' === $key || 'soort_lid' === $key || 'geboortedatum' === $key) {
+                    if ('geboortedatum' == $key) {
+                        $row = $obj_output_adjustment->mtd_human_readable_date($row);
                     }
+                    $familieleden[$key] = $row;
                 }
             }
-            if (!empty($records['familienaam']) && !empty($records['familie_id']) && isset($familieleden)) {
-                $familie_overzicht[$records['adres']]['familienaam'] = $records['familienaam'];
-                $familie_overzicht[$records['adres']]['familie_id'] = $records['familie_id'];
-                $familie_overzicht[$records['adres']]['familieleden'][] = $familieleden;
-            }
+            $familie_overzicht[$records['adres']]['familienaam'] = $records['familienaam'];
+            $familie_overzicht[$records['adres']]['familie_id'] = $records['familie_id'];
+            $familie_overzicht[$records['adres']]['familieleden'][] = $familieleden;
         }
-        if (isset($familie_overzicht)) {
-            return $familie_overzicht;
-        } else {
-            return false;
-        }
+        return $familie_overzicht;
     }
 }
