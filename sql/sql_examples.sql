56fcd0dc23041acf8713e69a4ec0e2ea0f2493e2
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-10 23:35:36 +0100

    Update sql_examples.sql

--- sql/sql_examples.sql
+++ sql/sql_examples.sql
@@ -11,7 +11,7 @@ ORDER BY n.date_created DESC
 /* SQL Example 2: Create a view */
 CREATE VIEW `Makeup View` AS
 SELECT ID,post_title, post_content, post_date 
-FROM wp_66qfw1nv3v_posts
+FROM wp_posts
 WHERE post_content LIKE '%makeup%'
 AND post_status = "publish"
 ORDER BY post_date DESC
