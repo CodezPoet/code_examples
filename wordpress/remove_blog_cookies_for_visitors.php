34839e923555e9e6186fa3080f7357be92b4241d
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-16 20:19:14 +0100

    Update remove_blog_cookies_for_visitors.php

--- wordpress/remove_blog_cookies_for_visitors.php
+++ wordpress/remove_blog_cookies_for_visitors.php
@@ -8,8 +8,8 @@ add_action('init', 'remove_blog_cookies');
 
 function remove_blog_cookies()
 {
-    if (! is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php') {
-        if (! empty($_COOKIE)) {
+    if (!is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php') {
+        if (!empty($_COOKIE)) {
             foreach ($_COOKIE as $cookie_name => $cookie) {
                 unset($cookie);
                 unset($_COOKIE[$cookie_name]);
