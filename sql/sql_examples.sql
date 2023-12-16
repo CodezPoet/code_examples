a37f81d7294ca18d8d66cd5abb08fa8231a58721
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-16 16:02:27 +0100

    Update sql_examples.sql

--- sql/sql_examples.sql
+++ sql/sql_examples.sql
@@ -1,5 +1,4 @@
 /* SQL Example 1: Joined query */
-
 SELECT tn.custom_field_2, cf.custom_field_3, group_concat(n.custom_field_4 separator ', '), group_concat(nr.custom_field_5 separator ', ')
 FROM database_1.table_1 n
   LEFT JOIN database_1.table_2 tn ON n.nid = tn.nid
@@ -9,8 +8,18 @@ WHERE n.custom_field_8 = '%d'
 GROUP BY n.custom_field_10
 ORDER BY n.date_created DESC
 
-/* SQL Example 2: Create a view */
-  
+/* SQL Example 2: Compare two columns of the same name in two different tables and show the unmatched records */
+SELECT phrase 
+FROM (
+Select phrase From table_1
+    UNION ALL 
+Select phrase From table_2
+) tbl
+GROUP BY phrase
+HAVING count(*) = 1
+ORDER BY phrase;
+
+/* SQL Example 3: Create a view */
 CREATE VIEW `Makeup View` AS
 SELECT ID, post_title, post_content, post_date 
 FROM wp_posts
