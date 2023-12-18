17e01c2afce804a35fb03be7f3d3720f806df8b5
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-18 23:07:49 +0100

    Update bmi_calculator.php

--- php/bmi_calculator.php
+++ php/bmi_calculator.php
@@ -1,20 +1,33 @@
-<?php 
-
 // file with height and weight
-require('measurements.php');
-
-// Function to calculate BMI
-function calculateBMI($weightMetric, $heightMetric)
+require_once('measurements.php');
+ 
+/**
+ * Function to calculate BMI
+ * 
+ * @param float|null $weightMetric
+ * @param float|null $heightMetric
+ * 
+ * @return float
+ */
+function calculateBMI(?float $weightMetric = null, ?float $heightMetric = null): float
 {
     $bmi = $weightMetric / ($heightMetric * $heightMetric);
     $bmi = number_format($bmi, 2);
-   
+
     return $bmi;
 }
 
-// Determine BMI Category
-function bmiCategory($bmi)
+/**
+ * Determine BMI Category
+ * 
+ * @param float|null $bmi
+ * 
+ * @return string
+ */
+function bmiCategory(?float $bmi = null): string
 {
+    $bmiCategory = '';
+
     switch (true) {
         case ($bmi < 18.5):
             $bmiCategory = 'Underweight';
@@ -29,25 +42,31 @@ function bmiCategory($bmi)
             $bmiCategory = 'Obese';
             break;
     }
-    
+
     return $bmiCategory;
 }
 
-// BMI result
-function bmiResultMetric($heightMetric = '', $weightMetric = '')
+/**
+ * BMI result
+ * 
+ * @param float|null $heightMetric
+ * @param float|null $weightMetric
+ * 
+ * @return array
+ */
+function bmiResultMetric(?float $heightMetric = null, ?float $weightMetric = null): array
 {
+    $bmiResult = [];
     if (0 < $heightMetric && 4 > $heightMetric && 0 < $weightMetric && 1000 > $weightMetric) {
         $bmi = calculateBMI($weightMetric, $heightMetric);
         $bmiCategory = bmiCategory($bmi);
-        $bmiResult = array(
+        $bmiResult = [
             'weight_metric' => $weightMetric,
             'height_metric' => $heightMetric,
-            'bmi'           => $bmi,
-            'bmi_category'  => $bmiCategory,
-        );
-       
+            'bmi' => $bmi,
+            'bmi_category' => $bmiCategory,
+        ];
+
         return $bmiResult;
-    } else {
-        return false;
     }
 }
