b5de82191fb0b92b1d49fcb520780c5843f423f3
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-21 02:12:45 +0100

    Add files via upload

--- .net-core/JokesWebApp/obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
+++ .net-core/JokesWebApp/obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-JokesWebApp-b066e375-3662-450f-a3ca-0792d761f727")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
