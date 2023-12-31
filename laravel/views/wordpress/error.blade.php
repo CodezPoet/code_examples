73f65e6c32f04a105dcd2e64496241db24c72a79
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2023-12-31 10:03:49 +0100

    Create error.blade.php

---
+++ laravel/views/wordpress/error.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title', 'WordPress API Error')
+
+@section('content')
+<div id="data-wrapper">
+    <h2>WordPresss API Error</h2>
+    <div class="search-error">
+        <p>There is an error with the result from the API</p>
+    </div>
+</div>
+@stop
