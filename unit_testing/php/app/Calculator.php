d8b8035099cf168a92c235b8a30ea905f5a0ebd6
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2024-01-21 22:23:22 +0100

    Update Calculator.php

--- unit_testing/php/app/Calculator.php
+++ unit_testing/php/app/Calculator.php
@@ -2,21 +2,57 @@
 
 namespace App;
 
-class Calculator{
+/**
+ * Calculator Class
+ */
+class Calculator
+{
     
-    public function add($num1, $num2) {
+    /**
+     * Calculator Add method
+     * 
+     * @param float $num1
+     * @param float $num2
+     * 
+     * @return int
+     */
+    public function add(float $num1, float $num2): float {
         return $num1 + $num2;
     }
 
-    public function subtract($num1, $num2) {
+    /**
+     * Calculator Subtract Method
+     * 
+     * @param float $num1
+     * @param float $num2
+     * 
+     * @return int
+     */
+    public function subtract(float $num1, float $num2): float {
         return $num1 - $num2;
     }
 
-    public function multiply($num1, $num2) {
+    /**
+     * Calculator Multiply Method
+     * 
+     * @param float $num1
+     * @param float $num2
+     * 
+     * @return int
+     */
+    public function multiply(float $num1, float $num2): float {
         return $num1 * $num2;
     }
 
-    public function divide($num1, $num2) {
+    /**
+     * Calculator Divide Method
+     * 
+     * @param float $num1
+     * @param float $num2
+     * 
+     * @return int
+     */
+    public function divide(float $num1, float $num2): float {
         return $num1 / $num2;
     }
 }
