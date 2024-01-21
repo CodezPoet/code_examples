782d55f2180e3322a5e0e6ec659aa3ad44e2bba2
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2024-01-21 10:39:51 +0100

    Update SingleResponsibilityPrinciple.cs

--- solid_design_principles/c#/SingleResponsibilityPrinciple.cs
+++ solid_design_principles/c#/SingleResponsibilityPrinciple.cs
@@ -11,38 +11,31 @@ using static System.Console;
 
 namespace DesignPatterns
 {
-    // Class is only responsible for handling Journal entry
     public class Journal
     {
         private readonly List<string> entries = new List<string>();
 
         private static int count = 0;
 
-        // Adds a new entry to the journal
-        public int AddEntry(string text)
+       public int AddEntry(string text)
         {
             entries.Add($"{++count}: {text}");
             return count; // Memento
         }
 
-        // Removes an entry from the journal based on the provided index
-        public void RemoveEntry(int index)
+       public void RemoveEntry(int index)
         {
             entries.RemoveAt(index);
         }
 
-        // Returns a string representation of the journal entries
         public override string ToString()
         {
             return string.Join(Environment.NewLine, entries);
         }
     }
 
-    // Separation of concerns. Saving file in another class instead of the Journal class
-    // Following the Single Responsibility Principle
     public class Persistence
     {
-        // Saves the journal entries to a file
         public void SaveToFile(Journal j, string filename, bool overwrite = false)
         {
             if (overwrite || !File.Exists(filename))
@@ -52,7 +45,6 @@ namespace DesignPatterns
         }
     }
 
-    // Run demo code
     public class Demo
     {
         static void Main(string[] args)
@@ -61,14 +53,10 @@ namespace DesignPatterns
             j.AddEntry("I crushed a bug");
             j.AddEntry("I had a cookie today");
             WriteLine(j);
-
+      
             var p = new Persistence();
             var filename = @"c:\temp\journal.txt";
-
-            // Save journal entries to file
             p.SaveToFile(j, filename, true);
-
-            // Open the text file with the default text editor
             Process.Start("notepad.exe", filename);
         }
     }
