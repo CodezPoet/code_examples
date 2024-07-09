7bed2f7e2ebf0e189d611a35db819662d315fb42
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2024-07-09 09:39:55 +0800

    code formatting

--- cms/wordpress/remove_blog_cookies_for_visitors.php
+++ cms/wordpress/remove_blog_cookies_for_visitors.php
@@ -1,20 +1,21 @@
-<?php 
+<?php
 
 /* 
-* Remove all blog cookies for non logged in visitors
-* Can be put in functions.php of the active theme being used
-*/
-add_action('init', 'remove_blog_cookies');
+ * Remove all blog cookies for non logged in visitors
+ * Can be put in functions.php of the active theme being used
+ */
+add_action( 'init', 'remove_blog_cookies' );
 
 function remove_blog_cookies()
 {
-    if (!is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php') {
-        if (!empty($_COOKIE)) {
-            foreach ($_COOKIE as $cookie_name => $cookie) {
-                unset($cookie);
-                unset($_COOKIE[$cookie_name]);
+    if ( ! is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php' ) {
+        if ( ! empty( $_COOKIE ) ) {
+            foreach ( $_COOKIE as $cookie_name => $cookie ) {
+                unset( $cookie );
+                unset( $_COOKIE[ $cookie_name ] );
             }
         }
     }
+
     return $_COOKIE;
 }
