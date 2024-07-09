18400d92ce4519acce6050cada7e96a12a2da7af
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2024-07-09 09:40:53 +0800

    code formatting

--- cms/wordpress/search_and_replace_text_posts.php
+++ cms/wordpress/search_and_replace_text_posts.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 
 // Search and replace text in posts
-$search = 'speling eror';
+$search  = 'speling eror';
 $replace = 'correct spelling';
-$args = array(
+$args    = array(
     'post_type'      => 'post',
     'posts_per_page' => -1,
     's'              => $search,
 );
-$posts = new WP_Query($args);
-if ($posts->have_posts()) {
-    while ($posts->have_posts()) {
+$posts   = new WP_Query( $args );
+if ( $posts->have_posts() ) {
+    while ( $posts->have_posts() ) {
         $posts->the_post();
-        $post_content = get_the_content();
-        $updated_content = str_replace($search, $replace, $post_content);
-        wp_update_post(array(
-            'ID'           => get_the_ID(),
-            'post_content' => $updated_content,
-        ));
+        $post_content    = get_the_content();
+        $updated_content = str_replace( $search, $replace, $post_content );
+        wp_update_post(
+            array(
+                'ID'           => get_the_ID(),
+                'post_content' => $updated_content,
+            )
+        );
     }
     wp_reset_postdata();
 }
