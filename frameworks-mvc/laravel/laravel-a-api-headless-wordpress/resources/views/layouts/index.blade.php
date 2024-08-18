000c77be19fd8b1d4f8904eef3c41c8ad6e23c88
CodePoet <suppliers.tg@outlook.com>
2024-08-18 16:43:26 +0800

    Update file directory structure

---
+++ frameworks-mvc/laravel/laravel-a-api-headless-wordpress/resources/views/layouts/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'The List of Tasks')
+
+@section('content')
+    <div>
+        <a href="{{ route('tasks.create') }}">Add Task</a>
+    </div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+
+    @if ($tasks->count())
+        {{ $tasks->links() }}
+    @endif
+@endsection
