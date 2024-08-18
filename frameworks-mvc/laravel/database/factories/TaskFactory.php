e91b12d8af7f79ad2be880db35e926c7849ae828
CodezPoet <115930100+CodezPoet@users.noreply.github.com>
2024-08-18 16:22:23 +0800

    Create TaskFactory.php

---
+++ frameworks-mvc/laravel/database/factories/TaskFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+          'title' => fake()->sentence,
+          'description' => fake()->paragraph,
+          'long_description' => fake()->paragraph(7, true),
+          'completed' => fake()->boolean,
+        ];
+    }
+}
