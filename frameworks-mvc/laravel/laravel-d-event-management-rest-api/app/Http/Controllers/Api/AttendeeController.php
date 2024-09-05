6474e079c706a15a29dd972902c76e4039b778cf
CodePoet <suppliers.tg@outlook.com>
2024-09-05 15:26:09 +0800

    update code

--- frameworks-mvc/laravel/laravel-d-event-management-rest-api/app/Http/Controllers/Api/AttendeeController.php
+++ frameworks-mvc/laravel/laravel-d-event-management-rest-api/app/Http/Controllers/Api/AttendeeController.php
@@ -16,6 +16,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+        ->only('store', 'destroy');
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
@@ -39,7 +41,7 @@ class AttendeeController extends Controller
     public function store(Request $request, Event $event)
     {
         $attendee = $event->attendees()->create([
-            'user_id' => 1
+            'user_id' => $request->user()->id,
         ]);
 
         return new AttendeeResource($attendee);
@@ -66,7 +68,6 @@ class AttendeeController extends Controller
      */
     public function destroy(Event $event, Attendee $attendee)
     {
-        $this->authorize('delete-attendee', [$event, $attendee]);
         $attendee->delete();
 
         return response(status: 204);
